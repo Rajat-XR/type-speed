b92219529af52190c6f6a53f8d022f839eaf5e03
rajatjain01970 <rajatjain01970@gmail.com>
2024-10-10 20:49:54 +0000

    commit

--- scripts/logic.js
+++ scripts/logic.js
@@ -28,12 +28,13 @@ let random
 let arr = []
 
 document.addEventListener("DOMContentLoaded", () => {
+    settings_dialog.showModal()
     random = Math.floor(Math.random() * (max - min + 1) + min);
     words.push(...easy)
     display_word.innerHTML = words[random]
 
     level.onchange = function () {
-        if (this.value == "easy") {
+        if (this.value == "Easy Mode") {
             words.length = 0
             max = 3061
             random = Math.floor(Math.random() * (max - min + 1) + min);
@@ -42,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
             display_word.innerHTML = words[random]
         }
 
-        if (this.value == "medium") {
+        if (this.value == "Medium Mode") {
             words.length = 0
             max = 312
             random = Math.floor(Math.random() * (max - min + 1) + min);
@@ -51,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
             display_word.innerHTML = words[random]
         }
 
-        if (this.value == "hard") {
+        if (this.value == "Hard Mode") {
             words.length = 0
             max = 340
             random = Math.floor(Math.random() * (max - min + 1) + min);
@@ -59,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(words.length)
             display_word.innerHTML = words[random]
         }
-        if (this.value == "pro") {
+        if (this.value == "Pro Mode") {
             words.length = 0
             max = 92
             random = Math.floor(Math.random() * (max - min + 1) + min);
@@ -75,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     applybtn.onclick = () => {
         settings_dialog.close()
-        settingbtn.innerHTML = "Game Settings"
+        settingbtn.innerHTML = level.value
     }
     // if (localStorage.getItem("theme") != null) {
     //     for (let i = 0; i <= 3; i++) {
@@ -108,26 +109,32 @@ document.addEventListener("DOMContentLoaded", () => {
         user_input.value = ""
         flag = 0
         score.innerHTML = `Score: ${user_score}`
-        score.style.color = "yellow"
+        score.style.color = "black"
         timeleft.innerHTML = `Time: 5 seconds`
         random = Math.floor(Math.random() * (max - min + 1) + min);
         display_word.innerHTML = words[random]
+        settingbtn.disabled = false
+        settingbtn.style.opacity = "1"
+        settingbtn.innerHTML = "Difficulty & Theme Settings"
         result.close()
     }
     user_input.onkeyup = () => {
+        settingbtn.disabled = true
+        settingbtn.style.opacity = "0.8"
+        settingbtn.innerHTML = level.value
         if (flag == 0) {
             let a = setInterval(() => {
                 if (time === 0) {
                     if (user_score > localStorage.getItem("high")) {
                         localStorage.setItem("high", user_score)
-                        gameover_display.innerHTML = `🔥Great Game🔥 [New HIGH Score: ${user_score} 💪]`
+                        gameover_display.innerHTML = `GAME OVER [🔥 NEW HIGH SCORE 🔥]`
                         highscore.innerHTML = `High Score: ${localStorage.getItem("high")}`
                     }
                     else {
                         gameover_display.innerHTML = "GAME OVER"
                     }
-                    gameover_score.innerHTML = `Score: ${user_score}`
-                    gameover_high.innerHTML = `High: ${localStorage.getItem("high")}`
+                    gameover_score.innerHTML = `Score: ${user_score} (in ${level.value})`
+                    gameover_high.innerHTML = `High Score: ${localStorage.getItem("high")}`
                     result.showModal()
                     clearInterval(a);
                     arr.push(user_score)
